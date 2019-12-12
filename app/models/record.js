cab2fb3b125368e9e71d861c3b32647ed8d05da9
loic.flagollet <loic.flagollet@augure.com>
2019-12-12 15:49:08 +0100

    Add new columns to RecordSchema

--- app/models/record.js
+++ app/models/record.js
@@ -8,17 +8,19 @@ const Schema = mongoose.Schema;
 const RecordSchema = new Schema(
   {
     submissionDateTimeUtc: { type: Date },
-    benchmarkVersion: { type: Number, min: 1 },
-    benchmarkSceneId: { type: String },
-    cpuModel: { type: String },
+    benchmarkVersion: { type: Number, min: 1, index: true },
+    benchmarkSceneId: { type: String, index: true },
+    cpuModel: { type: String, index: true },
+    cpuCoreCount: { type: Number, index: true },
+    cpuThreadCount: { type: Number, index: true },
+    renderThreadCount: { type: Number, index: true },
+    operatingSystem: { type: String, index: true },
     renderTime: { type: Number },
   },
   {
     versionKey: false,
   },
 );
-
-RecordSchema.index({ benchmarkVersion: 1, benchmarkSceneId: 1, cpuModel: 1 });
 
 /**
  * Add your
